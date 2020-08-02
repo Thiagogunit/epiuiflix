a4af7cd9615b197a19400653fe047ec459c8043f
Thiagogunit <thiagog-unit@hotmail.com>
2020-08-02 01:06:22 -0300

    setup 2 do projeto

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -8,10 +8,10 @@ function Footer() {
         <img src="https://www.alura.com.br/assets/img/alura-logo-white.1570550707.svg" alt="Logo Alura" />
       </a>
       <p>
-        Orgulhosamente criado durante a
+        Orgulhosamente criado por Thiguinha 
         {' '}
         <a href="https://www.alura.com.br/">
-          Imersão React da Alura
+          no Imersão React da Alura
         </a>
       </p>
     </FooterBase>
--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 import logo from '../../assets/img/logo.png';
 import './menu.css';
 //import ButtonLink from './components/ButtonLink';
@@ -7,10 +8,11 @@ import Button from '../Button';
 function Menu(){
     return (
         <nav className ="Menu">
-            <a href="/">
+            <Link to="/">
         <img className ="logo" src ={logo} alt="EpíuiFlix logo" />
-        </a>
-        <Button as="a" className="ButtonLink" href="/">
+        </Link>
+
+        <Button as={Link} className="ButtonLink" to="/cadastro/video">
         Novo Vídeo
         </Button>
 
